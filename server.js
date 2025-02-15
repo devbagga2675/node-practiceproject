4a992fcd58daf613bd34f81cd1961e6e5e09ae3d
devbagga2675 <devbagga2675@gmail.com>
2025-02-15 18:30:16 +0530

    installed nodemon and express

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const e = require('express');
+const dotenv = require('dotenv').config();
+
 const app = e();
-const port = 5000;
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
-    console.log(`listening on port: ${port}`);
+    console.log(`still listening on port: ${port}`);
 })
 
